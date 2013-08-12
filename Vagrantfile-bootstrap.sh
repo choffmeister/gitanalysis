61991a4d33c505f395e2cead10bb4770ac631e9b
Christian Hoffmeister <mail@choffmeister.de>
2013-08-12 23:13:55 +0200

    Improve vagrant bootstrap

--- Vagrantfile-bootstrap.sh
+++ Vagrantfile-bootstrap.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
+# update packages
 sudo aptitude update
-sudo aptitude install --assume-yes mono-devel mono-gmcs nunit-console
+
+# install vim
+sudo aptitude install vim
+
+# install build dependencies for libgit2
 sudo aptitude install --assume-yes git
-sudo aptitude install --assume-yes build-essential cmake
-sudo aptitude install --assume-yes build-essential libssl-dev
+sudo aptitude install --assume-yes build-essential
+sudo aptitude install --assume-yes cmake
+sudo aptitude install --assume-yes libssl-dev
+
+# install mono
+sudo aptitude install --assume-yes mono-devel mono-gmcs nunit-console
+mozroots --import --sync
+
+# build libgit2 (9d9fff3)
+git clone git://github.com/libgit2/libgit2.git
+cd libgit2
+git checkout 9d9fff3
+mkdir build
+cd build
+cmake ..
+cmake --build
+
+# install libgit2 (9d9fff3)
+sudo make install
+sudo ln -s /usr/local/lib/libgit2.so.0.18.0 /usr/local/lib/libgit2-9d9fff3.so
